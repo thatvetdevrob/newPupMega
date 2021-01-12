7e0e9aa1e1f8682ae0e5e3402335ee1d7dfa002a
thatvetdevrob <thatvetdevrob@gmail.com>
2021-01-11 20:20:25 -0500

    some basic styling

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # newPupMega
-Pup Mega: now in react
+
+Pup Mega: now in react app!
+
+
+# Credits:
+
+These are the sources that make my react app awesome!
+
+- Bone image from : http://www.onlinewebfonts.com
+
+- SVG background from: https://www.svgbackgrounds.com
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './bone.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          New Pup Mega will be up soon...
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://thatvetdevrob.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My dev portfolio
         </a>
       </header>
     </div>
